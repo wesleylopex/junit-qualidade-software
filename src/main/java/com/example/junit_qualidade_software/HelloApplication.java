e8a951a395ce2e7e4ff11280b5b6c908ccf48c28
Alisson Costa Schmidt <acschmidt@inf.ufsm.br>
2023-04-19 14:30:39 -0300

    add layout design

--- src/main/java/com/example/junit_qualidade_software/HelloApplication.java
+++ src/main/java/com/example/junit_qualidade_software/HelloApplication.java
@@ -1,22 +1,37 @@
 package com.example.junit_qualidade_software;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class HelloApplication extends Application {
+    @FXML
+    private ComboBox<String> comboBox;
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<String> items = FXCollections.observableArrayList("Item 1", "Item 2", "Item 3");
+        comboBox.setItems(items);
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Scene scene = new Scene(root, 600, 502);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
         launch();
     }
