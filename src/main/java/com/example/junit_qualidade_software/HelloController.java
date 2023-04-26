2c5cfa9773cb6c6fa1ee1e308b958441eca7c4f8
Alisson Costa Schmidt <acschmidt@inf.ufsm.br>
2023-04-26 17:58:26 -0300

    Tentativa de usar Singleton

--- src/main/java/com/example/junit_qualidade_software/HelloController.java
+++ src/main/java/com/example/junit_qualidade_software/HelloController.java
@@ -3,19 +3,32 @@ package com.example.junit_qualidade_software;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 public class HelloController {
+
+    //private static HelloController instance;
+    public HelloController() {}
+
+    /*
+    * TODO: achar onde o controller é instanciado.
+    * Aparentemente, tem uma chamada interna (new HelloController()) em algum lugar,
+    * então, ao usar o singleton, essa chamada é impedidda, pois deveria ser: HelloController.getInstance();.
+    */
+
+    /*public static synchronized HelloController getInstance() {
+        if (instance == null) {
+            instance = new HelloController();
+        }
+        return instance;
+    }*/
+
     @FXML
     private ComboBox<String> comboBox;
     @FXML
-    private TextField tfNome;
+    private TextField tfNome, tfCpf, tfEmail, tfSenha, tfConfirm_senha;
+    private String nome, cpf_cnpj, email, senha, confir_senha;
 
     @FXML
     void initialize() {
@@ -23,13 +36,34 @@ public class HelloController {
         ObservableList<String> items = FXCollections.observableArrayList("Item 1", "Item 2", "Item 3");
         comboBox.setItems(items);
     }
-    protected String read_name() {
-        return tfNome.getText();
-    }
 
     @FXML
     protected void onProntoButtonClick() {
-        System.out.println(read_name());
+        this.nome = tfNome.getText();
+        this.cpf_cnpj = tfCpf.getText();
+        this.email = tfEmail.getText();
+        this.senha = tfSenha.getText();
+        this.confir_senha = tfConfirm_senha.getText();
         TesteInterface.main(this);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf_cnpj() {
+        return cpf_cnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getConfir_senha() {
+        return confir_senha;
     }
 }
--- src/main/java/com/example/junit_qualidade_software/TesteInterface.java
+++ src/main/java/com/example/junit_qualidade_software/TesteInterface.java
@@ -1,11 +1,18 @@
 package com.example.junit_qualidade_software;
 import junit.framework.TestCase;
-import com.example.junit_qualidade_software.HelloController;
 
 
 public class TesteInterface extends TestCase {
-    public static void main(HelloController hello) {
-        assertEquals("Alisson", hello.read_name());
+    public static void main(HelloController controller) {
+        //HelloController controller = HelloController.getInstance();
+
+        // TODO: testar se existe/está aparecendo
+
+        assertEquals("Alisson", controller.getNome());
+        assertEquals("12345678910", controller.getCpf_cnpj());
+        assertEquals("email@gmail.com", controller.getEmail());
+        assertEquals("senha123", controller.getSenha());
+        assertEquals("senha123", controller.getConfir_senha());
         System.out.println();
     }
 
